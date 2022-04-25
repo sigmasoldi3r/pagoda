77f0b597ce932d366ca1502d4d553643feb93e7f
sigmasoldi3r <pablobc.1995@gmail.com>
2022-04-25 12:43:44 +0200

    Added title and options to editor.

--- src/lib/useOptionState.ts
+++ src/lib/useOptionState.ts
@@ -1,6 +1,21 @@
 import { none, option, some } from '@octantis/option'
 import { useState } from 'react'
 
+export type SetStateAction<A> = ((prev: option<A>) => A) | A
+export type ResetStateAction<A> = void | ((prev: option<A>) => void)
+
+export function isDispatch<R>(
+  value: SetStateAction<R>
+): value is (prev: option<R>) => R {
+  return typeof value === 'function'
+}
+
+export function isReset<R>(
+  value: ResetStateAction<R>
+): value is (prev: option<R>) => void {
+  return typeof value === 'function'
+}
+
 /**
  * Creates a state that is of type option<A> automatically,
  * and returns two additional mutator functions: setState which
@@ -13,8 +28,23 @@ export default function useOptionState<A>(...initial: [A] | []) {
   const [state, setState] = useState<option<A>>(init)
   return [
     state,
-    (value: A) => setState(() => some(value)),
-    () => setState(() => none()),
+    ((value: SetStateAction<A>) => {
+      if (isDispatch(value)) {
+        setState(prev => some(value(prev)))
+      } else {
+        setState(() => some(value))
+      }
+    }) as React.Dispatch<SetStateAction<A>>,
+    ((value?: ResetStateAction<A>) => {
+      if (isReset(value)) {
+        setState(prev => {
+          value(prev)
+          return none()
+        })
+      } else {
+        setState(() => none())
+      }
+    }) as React.Dispatch<ResetStateAction<A>>,
     setState,
   ] as const
 }
--- src/menus/RomEditor.tsx
+++ src/menus/RomEditor.tsx
@@ -11,17 +11,15 @@ export interface RomEditorProps {
   rom?: Rom
 }
 
-function NoRom() {
-  return <div>We're loading your ROM...</div>
-}
-
-let editor: option<monaco.editor.IStandaloneCodeEditor> = none()
+type Editor = monaco.editor.IStandaloneCodeEditor
 
 // Rom project editor.
 export default function RomEditor({ rom: preloadedRom }: RomEditorProps) {
   const ref = useRef<HTMLDivElement>()
   const [rom, setRom] = useOptionState<Rom>()
+  const [editor, setEditor] = useOptionState<Editor>()
   const [editing, setEditing] = useState('init.pag')
+  const [content, setContent] = useState('')
   useEffect(() => {
     if (preloadedRom != null) {
       setRom(preloadedRom)
@@ -30,7 +28,13 @@ export default function RomEditor({ rom: preloadedRom }: RomEditorProps) {
     }
   }, [])
   useEffect(() => {
+    console.log('Do', editor, ref.current)
     if (editor.isEmpty() && ref.current != null) {
+      while (ref.current.firstChild) {
+        if (ref.current.lastChild != null) {
+          ref.current.removeChild(ref.current.lastChild)
+        }
+      }
       const ed = monaco.editor.create(ref.current, {
         theme: 'vs-dark',
         model: monaco.editor.createModel(
@@ -47,47 +51,64 @@ Feel free to edit.
         wrappingStrategy: 'advanced',
       })
       ed.layout()
-      editor = some(ed)
+      setContent(() => ed.getValue())
+      setEditor(last => {
+        for (const editor of last) {
+          editor.dispose()
+        }
+        return ed
+      })
+      ed.onDidChangeModelContent(() => {
+        setContent(() => ed.getValue())
+      })
     }
   }, [rom])
-  return rom.fold(<NoRom />, rom => {
-    const scripts = Object.entries(rom.scripts)
-    const assets = Object.entries(rom.assets)
-    return (
-      <div style={{ height: '100%' }}>
-        <div className="editor">
-          <div className="editor-items">
-            <h3>ROM Content</h3>
-            <small>{rom.name}</small>
-            <hr />
-            <div>
-              <h4>Scripts ({scripts.length})</h4>
-              <ul className="list">
-                {scripts.map(([key, script], i) => (
-                  <li key={`_rom_script_${i}`} className="entry">
-                    {key} ({script.length} bytes)
-                  </li>
-                ))}
-              </ul>
-              <Button>
-                <Icon src={addScript} /> Create Script
-              </Button>
-            </div>
-            <div>
-              <h4>Assets ({assets.length})</h4>
-              <ul className="list">
-                {assets.map(([key, asset], i) => (
-                  <li key={`_rom_asset_${i}`} className="entry">
-                    {key} ({asset.length} bytes)
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <h2></h2>
-          <div className="editor-panel" ref={ref as any}></div>
+  useEffect(() => {}, [editor])
+  return (
+    <div style={{ height: '100%' }}>
+      <div className="editor">
+        <div className="editor-items">
+          {editor.zip(rom).fold(<></>, ([editor, rom]) => {
+            const scripts = Object.entries(rom.scripts)
+            const assets = Object.entries(rom.assets)
+            return (
+              <>
+                <h3>ROM Content</h3>
+                <small>{rom.name}</small>
+                <p>SLOC: {content.length}</p>
+                <hr />
+                <div>
+                  <h4>Scripts ({scripts.length})</h4>
+                  <ul className="list">
+                    {scripts.map(([key, script], i) => (
+                      <li key={`_rom_script_${i}`} className="entry">
+                        {key} ({script.length} bytes)
+                      </li>
+                    ))}
+                  </ul>
+                  <Button>
+                    <Icon src={addScript} /> Create Script
+                  </Button>
+                </div>
+                <div>
+                  <h4>Assets ({assets.length})</h4>
+                  <ul className="list">
+                    {assets.map(([key, asset], i) => (
+                      <li key={`_rom_asset_${i}`} className="entry">
+                        {key} ({asset.length} bytes)
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </>
+            )
+          })}
+        </div>
+        <div className="editor-panel">
+          <p>Editing {editing}</p>
+          <div className="editor-instance" ref={ref as any}></div>
         </div>
       </div>
-    )
-  })
+    </div>
+  )
 }

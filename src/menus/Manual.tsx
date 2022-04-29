9907f9c45920ffa7787b239ab5198ad09fc5e57f
sigmasoldi3r <pablobc.1995@gmail.com>
2022-04-29 18:00:38 +0200

    Updated manual.

--- src/menus/Manual.tsx
+++ src/menus/Manual.tsx
@@ -2,7 +2,6 @@
   This view contains the docs for developing Pagoda games.
   A guide about the engine, and a guide about the language.
 */
-import { none, option } from '@octantis/option'
 import { createContext, useContext, useEffect, useRef } from 'react'
 import * as shiki from 'shiki'
 import lock from '../components/Dialog/LockDialog'
@@ -28,7 +27,15 @@ export default function Manual() {
   const [hl, update] = useOptionState<shiki.Highlighter>()
   useEffect(() => {
     lock.safe('Loading manual...', async () => {
-      const h = await shiki.getHighlighter({})
+      const h = await shiki.getHighlighter({
+        langs: [
+          {
+            id: 'pagoda',
+            path: '/pagoda.tmLanguage.json',
+            scopeName: 'source.pagoda',
+          },
+        ],
+      })
       update(h)
     })
   })
@@ -38,7 +45,54 @@ export default function Manual() {
     }
     return (
       <CodeContext.Provider value={h}>
-        Docs: <Code src="const f = x => x*2" />
+        <h1>Pagoda Manual</h1>
+        <p>
+          This page contains a summary of all that pagoda engine can offer,
+          ranging from a simple usage documentation to a fully extended,
+          in-depth guide about the language.
+        </p>
+        <h2>Basics</h2>
+        <p>...</p>
+        <h2>The Language</h2>
+        <p>
+          Pagoda Engine comes with a built-in, custom scripting language, an
+          ad-hoc solution for the narrative nature of the engine.
+        </p>
+        <p>
+          Programs are declared in this way, making them safe (As no malicious
+          code execution is possible), and easier to script, as the language is
+          designed to follow with the flow of the tale.
+        </p>
+        <p>
+          Also Pagoda uses markdown for text highlight, a very well-known
+          language that is used for simple documents.
+        </p>
+        <Code
+          src={`##
+# This is a sample Pagoda script
+##
+
+"Hello World!" # Show a "Hello world" message
+wait # Wait for user input`}
+          lang="bash"
+        />
+        <p>
+          Although it takes alot from the "programming world", the language is
+          intended to be readable at first glance, or almost for simple
+          statements.
+        </p>
+        <p>This means that if you read something like:</p>
+        <Code
+          src={`
+if inventory's 1st is nothing {
+  "Oh! Nothing there"
+}`}
+          lang="ada"
+        />
+        <p>
+          It means what you're thinking: If the 1st slot of the inventory
+          contains nothing, then show a "Nothing there" message.
+        </p>
       </CodeContext.Provider>
     )
   })
